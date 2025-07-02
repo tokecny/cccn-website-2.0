397a56be0fa42aee81901a2897abe650997a06a6
Thiti Chainiyom <97273062+tokecny@users.noreply.github.com>
2025-07-03 01:44:49 +0700

    Update contact.tsx
    - add cccn-lab-joining-details.pdf for downloading it when clicked on the text
    - add docx for revising
    - add email draft text
    - and minor change in padding and color

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -3,13 +3,12 @@
 export function Contact() {
   return (
     <section id="contact" className="">
-      <div className="text-center mb-10">
-        <h2 className="text-3xl font-semibold text-pink-600 mb-4">
+      <div className="text-center">
+        <h2 className="text-3xl font-semibold text-pink-500 mb-6">
           Contact Us
         </h2>
       </div>
 
-      <div className="container mx-auto px-6 grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
         <div className="h-[300px] w-full rounded-xl overflow-hidden shadow-md">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1041.437982519937!2d100.5370975686862!3d13.73392455276756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29f621d051795%3A0x66bffe068bf3a36c!2sSor%20Thor%20Building!5e0!3m2!1sen!2sth!4v1751246405881!5m2!1sen!2sth"
@@ -24,17 +23,29 @@ export function Contact() {
         </div>
 
         <div className="text-left text-gray-700">
-          <p className="mb-4 text-lg font-semibold">Email</p>
-          <p className="mb-6">cccn.lab@gmail.com</p>
+          <p className="text-md mb-4">
+            We’re always excited to welcome new minds passionate about science and collaboration.<br></br> 
+            <a className="text-pink-500 hover:underline transition" href="/cccn-lab-joining-details.pdf" download>Read through the details</a> — then drop us an email!
+          </p>
 
-          <p className="mb-4 text-lg font-semibold">Phone</p>
-          <p className="mb-6">(+66)-2-256-4000 ext.71108</p>
+          <p className="text-lg font-semibold">Email</p>
+          <p className="mb-4">
+            <a
+              href={`mailto:cccn.lab@gmail.com?subject=Join%20CCCN%20Lab&body=Hi%20CCCN%20Lab%2C%0A%0AMy%20name%20is%20[Your%20Name]%20and%20I%E2%80%99m%20interested%20in%20joining%20the%20lab.%0A%0APlease%20find%20attached%20my%20CV%2C%20one-page%20summary%2C%20and%20certificate.%0A%0ALooking%20forward%20to%20hearing%20from%20you!%0A%0ABest%20regards%2C%0A[Your%20Name]`}
+              className="hover:text-pink-500 hover:underline transition">
+              cccn.lab@gmail.com
+            </a>
+          </p>
 
-          <p className="mb-4 text-lg font-semibold">Location</p>
-          <p className="mb-1">Sor Thor Building, Chula Neuroscience Center</p>
+          <p className="text-lg font-semibold">Phone</p>
+          <p className="mb-4">(+66)-2-256-4000 ext.71108</p>
+
+          <p className="text-lg font-semibold">Location</p>
+          <p className="mb-2">Sor Thor Building, Chula Neuroscience Center</p>
           <p className="text-sm text-gray-500">
             1873 Ratchadamri Rd, Pathum Wan, Bangkok, 10330
           </p>
+
         </div>
       </div>
     </section>
