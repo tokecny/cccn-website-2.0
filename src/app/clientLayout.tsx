a30d9df8b4debdbb09e7fb0c959f5073c13801d2
Thiti Chainiyom <97273062+tokecny@users.noreply.github.com>
2025-07-05 18:06:28 +0700

    Update clientLayout.tsx
    - avoid Hydration failed because the server rendered HTML didn't match the client...

--- src/app/clientLayout.tsx
+++ src/app/clientLayout.tsx
@@ -1,19 +1,25 @@
 "use client";
 
 import Navbar from "@/components/navbar";
+import { useEffect, useState } from "react";
 
 export default function ClientLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [year, setYear] = useState<number | null>(null);
+
+  useEffect(() => {
+    setYear(new Date().getFullYear());
+  }, []);
   return (
     <>
       <Navbar />
       <main className="">{children}</main>
       <footer className="border-t text-sm text-gray-500 text-center py-6">
         <p>
-          &copy; {new Date().getFullYear()} CCCN Lab. All rights reserved.
+          {year} CCCN Lab. All rights reserved.
         </p>
       </footer>
     </>
