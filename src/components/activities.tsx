ea09b9e16bcbd5543704c8c9ec00f48b37401648
Thiti Chainiyom <97273062+tokecny@users.noreply.github.com>
2025-07-05 18:50:05 +0700

    Update activities.tsx
    - make the calendar looks great on tablet

--- src/components/activities.tsx
+++ src/components/activities.tsx
@@ -4,7 +4,7 @@ export function Activities() {
       <h2 id="activities" className="text-3xl font-semibold tracking-tight text-pink-500 mb-8 scroll-mt-24">
         Activities
       </h2>
-      <div className="mx-auto max-w-5xl aspect-[3/3] sm:h-[550px] sm:w-[800px] rounded-2xl overflow-hidden shadow-lg">
+      <div className="mx-auto max-w-5xl aspect-[4/3] sm:h-[550px] sm:w-[700px] rounded-2xl overflow-hidden shadow-lg">
         <iframe
           src="https://calendar.google.com/calendar/embed?src=9043de76e2f595533cbd71bd7c2f092e3fccb19784b5dfb82021bb11792e8820%40group.calendar.google.com&ctz=Asia%2FBangkok"
           className="w-full h-full"
