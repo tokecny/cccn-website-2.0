347bc9f9f6d877b2306c86aed2db4bab4993dc86
Thiti Chainiyom <97273062+tokecny@users.noreply.github.com>
2025-07-06 22:40:08 +0700

    Update layout.tsx
    - update meta in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,36 @@ export const metadata: Metadata = {
     default: "CCCN Lab",
     template: "%s – CCCN Lab",
   },
+  description:
+    "Curiosity. Critical Thinking. Creativity. Nerds!",
   icons: {
     icon: "/favicon-rounded.ico",
   },
+  openGraph: {
+    title: "CCCN Lab",
+    description:
+      "Curiosity. Critical Thinking. Creativity. Nerds!",
+    url: "https://cccn-website-20.vercel.app",
+    siteName: "CCCN Lab",
+    images: [
+      {
+        url: "https://cccn-website-20.vercel.app/og-image.png", 
+        width: 1200,
+        height: 630,
+        alt: "CCCN Lab",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "CCCN Lab",
+    description:
+      "Curiosity. Critical Thinking. Creativity. Nerds!",
+    images: ["https://cccn-website-20.vercel.app/og-image.png"],
+  },
 };
+
 
 export default function RootLayout({
   children,
